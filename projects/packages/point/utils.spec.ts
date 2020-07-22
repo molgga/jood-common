a78c33bee09e1c2ca009faa7dae130903fad8e0f
이윤석 <ys84@wemakeprice.com>
2020-07-22 13:41:32 +0900

    포인트 util 추가

---
+++ projects/packages/point/utils.spec.ts
@@ -0,0 +1,24 @@
+import { intersect, isIntersect } from "./utils";
+
+fdescribe("point utils", () => {
+  it("intersect", () => {
+    expect(intersect({ start: 0, end: 100 }, { start: 20, end: 120 })).toEqual({
+      start: 20,
+      end: 100,
+    });
+    expect(
+      intersect({ start: 120, end: 200 }, { start: 20, end: 120 })
+    ).toEqual({
+      start: 0,
+      end: 0,
+    });
+  });
+  it("isIntersect", () => {
+    expect(isIntersect({ start: 0, end: 100 }, { start: 20, end: 120 })).toBe(
+      true
+    );
+    expect(isIntersect({ start: 120, end: 200 }, { start: 20, end: 120 })).toBe(
+      false
+    );
+  });
+});
---
+++ projects/packages/point/utils.ts
@@ -0,0 +1,45 @@
+/**
+ * @packageDocumentation
+ * @module point
+ */
+
+interface IntersectRange {
+  start: number;
+  end: number;
+}
+
+/**
+ * 축 교차점 비교
+ * one 의 { start, end } 와 other 의 { start, end } 를 비교
+ * @param {IntersectRange} one
+ * @param {IntersectRange} other
+ * @returns {IntersectRange}
+ */
+export function intersect(
+  one: IntersectRange,
+  other: IntersectRange
+): IntersectRange {
+  if (one.start >= other.end || other.start >= one.end) {
+    return { start: 0, end: 0 };
+  }
+  const start = Math.max(one.start, other.start);
+  const end = Math.min(one.end, other.end);
+  if (end - start <= 0) {
+    return { start: 0, end: 0 };
+  }
+  return { start, end };
+}
+
+/**
+ * intersect 를 이용해 hitTest
+ * @param {IntersectRange} one
+ * @param {IntersectRange} other
+ * @returns {boolean}
+ */
+export function isIntersect(
+  one: IntersectRange,
+  other: IntersectRange
+): boolean {
+  const sect = intersect(one, other);
+  return sect.start !== 0 || sect.end !== 0;
+}

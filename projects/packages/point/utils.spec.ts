5eebd515ccbf9571b1128ed68eb135b449397341
이윤석 <ys84@wemakeprice.com>
2020-09-04 18:42:06 +0900

    [feature] point isCollision 추가

--- projects/packages/point/utils.spec.ts
+++ projects/packages/point/utils.spec.ts
@@ -1,4 +1,4 @@
-import { intersect, isIntersect } from "./utils";
+import { intersect, isIntersect, isCollision } from "./utils";
 
 describe("point utils", () => {
   it("intersect", () => {
@@ -13,6 +13,7 @@ describe("point utils", () => {
       end: 0,
     });
   });
+
   it("isIntersect", () => {
     expect(isIntersect({ start: 0, end: 100 }, { start: 20, end: 120 })).toBe(
       true
@@ -20,5 +21,26 @@ describe("point utils", () => {
     expect(isIntersect({ start: 120, end: 200 }, { start: 20, end: 120 })).toBe(
       false
     );
+  });
+
+  it("isCollision", () => {
+    let one = { x: 0, y: 0, w: 100, h: 100 };
+    let two = { x: 50, y: 50, w: 100, h: 100 };
+    expect(isCollision(one, two)).toBe(true);
+    one = { x: 0, y: 0, w: 100, h: 100 };
+    two = { x: 99, y: 99, w: 100, h: 100 };
+    expect(isCollision(one, two)).toBe(true);
+    one = { x: 0, y: 0, w: 100, h: 100 };
+    two = { x: 100, y: 100, w: 100, h: 100 };
+    expect(isCollision(one, two)).toBe(false);
+    one = { x: 0, y: 0, w: 100, h: 100 };
+    two = { x: 150, y: 150, w: 100, h: 100 };
+    expect(isCollision(one, two)).toBe(false);
+    one = { x: 100, y: 100, w: 100, h: 100 };
+    two = { x: 50, y: 50, w: 50, h: 50 };
+    expect(isCollision(one, two)).toBe(false);
+    one = { x: 100, y: 100, w: 100, h: 100 };
+    two = { x: 50, y: 50, w: 100, h: 100 };
+    expect(isCollision(one, two)).toBe(true);
   });
 });
--- projects/packages/point/utils.ts
+++ projects/packages/point/utils.ts
@@ -13,23 +13,17 @@
  * intersect({ start: 0, end: 100 }, { start: 20, end: 120 }); // { start: 20,end: 100 }
  * intersect({ start: 120, end: 200 }, { start: 20, end: 120 }); // { start: 0, end: 0 }
  */
-export function intersect(
-  one: IntersectRange,
-  other: IntersectRange
-): IntersectRange {
-  if (one.start >= other.end || other.start >= one.end) {
+export function intersect(one: IntersectRange, other: IntersectRange): IntersectRange {
+  if (other.end <= one.start || one.end <= other.start) {
     return { start: 0, end: 0 };
   }
   const start = Math.max(one.start, other.start);
   const end = Math.min(one.end, other.end);
-  if (end - start <= 0) {
-    return { start: 0, end: 0 };
-  }
   return { start, end };
 }
 
 /**
- * intersect 를 이용해 hitTest
+ * intersect 를 이용해 축 hitTest
  * @param {IntersectRange} one
  * @param {IntersectRange} other
  * @returns {boolean}
@@ -37,15 +31,32 @@ export function intersect(
  * isIntersect({ start: 0, end: 100 }, { start: 20, end: 120 }); // true
  * isIntersect({ start: 120, end: 200 }, { start: 20, end: 120 }); // true
  */
-export function isIntersect(
-  one: IntersectRange,
-  other: IntersectRange
-): boolean {
+export function isIntersect(one: IntersectRange, other: IntersectRange): boolean {
   const sect = intersect(one, other);
   return sect.start !== 0 || sect.end !== 0;
+}
+
+/**
+ * intersect 를 이용해 면 hitTest
+ * @param {CollisionBound} one
+ * @param {CollisionBound} other
+ * @returns {boolean}
+ * @example
+ */
+export function isCollision(one: CollisionBound, other: CollisionBound): boolean {
+  const hitX = isIntersect({ start: one.x, end: one.x + one.w }, { start: other.x, end: other.x + other.w });
+  const hitY = isIntersect({ start: one.y, end: one.y + one.h }, { start: other.y, end: other.y + other.h });
+  return hitX && hitY;
 }
 
 interface IntersectRange {
   start: number;
   end: number;
+}
+
+interface CollisionBound {
+  x: number;
+  y: number;
+  w: number;
+  h: number;
 }

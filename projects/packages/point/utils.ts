ec9bf2bd4d5e518fb89c77736310628dbca637e7
이윤석 <ys84@wemakeprice.com>
2020-09-04 18:57:05 +0900

    [refactor] example 코드 보완

--- projects/packages/point/utils.ts
+++ projects/packages/point/utils.ts
@@ -42,6 +42,12 @@ export function isIntersect(one: IntersectRange, other: IntersectRange): boolean
  * @param {CollisionBound} other
  * @returns {boolean}
  * @example
+ * let one = { x: 0, y: 0, w: 100, h: 100 };
+ * let two = { x: 50, y: 50, w: 100, h: 100 };
+ * isCollision(one, two); // true
+ * one = { x: 0, y: 0, w: 100, h: 100 };
+ * two = { x: 100, y: 100, w: 100, h: 100 };
+ * isCollision(one, two)); // false
  */
 export function isCollision(one: CollisionBound, other: CollisionBound): boolean {
   const hitX = isIntersect({ start: one.x, end: one.x + one.w }, { start: other.x, end: other.x + other.w });

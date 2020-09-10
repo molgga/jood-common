9d358b09faf35f8150a1e40372eef82114d57a37
이윤석 <ys84@wemakeprice.com>
2020-09-10 19:13:49 +0900

    [fix] browser-scroll observable optimize, add x

--- projects/packages/web/browser-scroll/BrowserScroll.ts
+++ projects/packages/web/browser-scroll/BrowserScroll.ts
@@ -1,5 +1,6 @@
 import { fromEvent, Subscription, Observable, Subject } from 'rxjs';
-import { IScrollState, ScrollDirection } from './types';
+import { filter, map } from 'rxjs/operators';
+import { IScrollState, ScrollDirection, ScrollType } from './types';
 
 /**
  * 브라우져(window) 스크롤
@@ -9,148 +10,127 @@ import { IScrollState, ScrollDirection } from './types';
 export class BrowserScroll {
   protected handleScroll: () => void;
   protected handleResize: () => void;
-  protected resizeEventName = 'resize';
-  protected scrollEventName = 'scroll';
   protected resizeObserver: Observable<Event>;
   protected scrollObserver: Observable<Event>;
-  protected scrollSubject: Subject<IScrollState>;
   protected eventListener: Subscription;
-
-  protected currentY = 0;
+  protected subjectScroll: Subject<ScrollType>;
+  protected observableScroll: Observable<ScrollType>;
+  protected yCurrent = 0;
   protected yDirection = 0;
   protected yDirectionLooseBefore = 0;
   protected yDirectionLooseGap = 20;
-  protected yDirectionSubject: Subject<IScrollState>;
-  protected yDirectionLooseSubject: Subject<IScrollState>;
-
   protected yEndDispatchHold = false;
-  protected yEndSubject: Subject<IScrollState>;
+  protected xCurrent = 0;
+  protected xDirection = 0;
+  protected xDirectionLooseBefore = 0;
+  protected xDirectionLooseGap = 20;
+  protected xEndDispatchHold = false;
 
-  protected yHalfEndDispatchHold = false;
-  protected yHalfEndSubject: Subject<IScrollState>;
+  constructor() {
+    this.eventListener = new Subscription();
+    this.subjectScroll = new Subject();
+    this.observableScroll = this.subjectScroll.asObservable();
+  }
 
   /**
    * 초기화
    */
   init(): void {
-    this.scrollSubject = new Subject();
-    this.yDirectionSubject = new Subject();
-    this.yDirectionLooseSubject = new Subject();
-    this.yEndSubject = new Subject();
-    this.yHalfEndSubject = new Subject();
-    this.eventListener = new Subscription();
+    this.initScroll();
+    this.initResize();
+  }
 
+  protected initScroll() {
     this.handleScroll = this.onScroll.bind(this);
-    this.handleResize = this.onResize.bind(this);
-    this.resizeObserver = fromEvent(window, this.resizeEventName);
-    this.scrollObserver = fromEvent(window, this.scrollEventName);
+    this.scrollObserver = fromEvent(window, 'scroll');
     this.eventListener.add(this.scrollObserver.subscribe(this.handleScroll));
+  }
+
+  protected initResize() {
+    this.handleResize = this.onResize.bind(this);
+    this.resizeObserver = fromEvent(window, 'resize');
     this.eventListener.add(this.resizeObserver.subscribe(this.handleResize));
   }
 
-  /**
-   * 옵저버블: 스크롤이 발생하면 알림
-   * @returns {Observable<IScrollState>}
-   */
-  observeScroll(): Observable<IScrollState> {
-    return this.scrollSubject.asObservable();
+  get scrollTop(): number {
+    return document.documentElement.scrollTop || document.body.scrollTop;
+  }
+
+  get scrollLeft(): number {
+    return document.documentElement.scrollLeft || document.body.scrollLeft;
+  }
+
+  get scrollHeight(): number {
+    return document.documentElement.scrollHeight || document.body.scrollHeight;
+  }
+
+  get scrollWidth(): number {
+    return document.documentElement.scrollWidth || document.body.scrollWidth;
+  }
+
+  get innerHeight(): number {
+    return window.innerHeight;
+  }
+
+  get innerWidth(): number {
+    return window.innerWidth;
   }
 
   /**
-   * 옵저버블: 스크롤의 방향이 바뀌면 알림
-   * @returns {Observable<IScrollState>}
+   * 구독자에게 전달되는 데이터
+   * @returns {IScrollState}
    */
-  observeDirectionY(): Observable<IScrollState> {
-    return this.yDirectionSubject.asObservable();
+  getState(): IScrollState {
+    const scrollTop = this.scrollTop;
+    const scrollHeight = this.scrollHeight;
+    const innerHeight = this.innerHeight;
+    const directionY = this.yDirection;
+    const holdEndY = this.yEndDispatchHold;
+    const endY = scrollHeight - innerHeight;
+    let percentY = scrollTop / endY;
+    percentY = isNaN(percentY) ? 0 : percentY;
+
+    const scrollLeft = this.scrollLeft;
+    const scrollWidth = this.scrollWidth;
+    const innerWidth = this.innerWidth;
+    const directionX = this.xDirection;
+    const holdEndX = this.xEndDispatchHold;
+    const endX = scrollWidth - innerWidth;
+    let percentX = scrollLeft / endX;
+    percentX = isNaN(percentX) ? 0 : percentX;
+
+    return {
+      scrollTop,
+      scrollHeight,
+      innerHeight,
+      directionY,
+      holdEndY,
+      percentY,
+      endY,
+      scrollLeft,
+      scrollWidth,
+      innerWidth,
+      directionX,
+      holdEndX,
+      percentX,
+      endX,
+    };
   }
 
   /**
-   * 옵저버블: 스크롤의 방향이 바뀌고, 일정 조건에 도달시 발생하는 알림
-   * @returns {Observable<IScrollState>}
-   */
-  observeDirectionLooseY(): Observable<IScrollState> {
-    return this.yDirectionLooseSubject.asObservable();
-  }
-
-  /**
-   * 옵저버블: 스크롤이 맨 아래에 도달할 때 알림
-   * @returns {Observable<IScrollState>}
-   */
-  observeEndY(): Observable<IScrollState> {
-    return this.yEndSubject.asObservable();
-  }
-
-  /**
-   * 옵저버블: 스크롤이 맨 아래에서 화면의 반을 뺀 위치에 도달할 때 알림
-   * @returns {Observable<IScrollState>}
-   */
-  observeHalfEndY(): Observable<IScrollState> {
-    return this.yHalfEndSubject.asObservable();
-  }
-
-  /**
-   * 트리거: observeScroll() 의 구독자들에게 알림
-   */
-  dispatchScroll(): void {
-    this.scrollSubject.next(this.getState());
-  }
-
-  /**
-   * 트리거: observeDirectionY() 의 구독자들에게 알림
-   */
-  dispatchDirectionY(): void {
-    this.yDirectionSubject.next(this.getState());
-  }
-
-  /**
-   * 트리거: observeDirectionLooseY() 의 구독자들에게 알림
-   */
-  dispatchDirectionLooseY(): void {
-    this.yDirectionLooseSubject.next(this.getState());
-  }
-
-  /**
-   * 트리거: observeEndY() 의 구독자들에게 알림
-   */
-  dispatchEndY(): void {
-    this.yEndSubject.next(this.getState());
-  }
-
-  /**
-   * 트리거: observeHalfEndY() 의 구독자들에게 알림
-   */
-  dispatchHalfEndY(): void {
-    this.yHalfEndSubject.next(this.getState());
-  }
-
-  /**
-   * 스크롤이 end 위치 보다 커지면 observeEndY() 으로 알림이 전달되는데,
-   * 연속해서 알림이 전달 되는것을 막기 위해 hold 상태로 제어를 하는데,
-   * 강제로 hold 상태를 제어해야 하는 경우에만 사용.
-   * @param {boolean} is
-   */
-  holdDispatchEndY(is: boolean): void {
-    this.yEndDispatchHold = is;
-  }
-
-  /**
-   * 스크롤이 end - 화면 높이반 위치 보다 커지면 observeHalfEndY() 으로 알림이 전달되는데,
-   * 연속해서 알림이 전달 되는것을 막기 위해 hold 상태로 제어를 하는데,
-   * 강제로 hold 상태를 제어해야 하는 경우에만 사용.
-   * @param {boolean} is
-   */
-  holdDispatchHalfEndY(is: boolean): void {
-    this.yHalfEndDispatchHold = is;
-  }
-
-  /**
-   * observeDirectionLooseY() 의 옵션.
-   * 스크롤 방향 변경에 대한 알림 조건 중,
-   * 이전 스크롤 위치와 현재 스크롤 위치의 여백 차이를 비교하는데, 그 차이값을 지정.
+   * 스크롤 loose Y 방향 변경에 대한 알림 조건 중, 이전 스크롤 위치와 현재 스크롤 위치의 여백 차이값
    * @param {number} gap
    */
   setDirectionLooseGapY(gap: number): void {
     this.yDirectionLooseGap = gap;
+  }
+
+  /**
+   * 스크롤 loose X 방향 변경에 대한 알림 조건 중, 이전 스크롤 위치와 현재 스크롤 위치의 여백 차이값
+   * @param {number} gap
+   */
+  setDirectionLooseGapX(gap: number): void {
+    this.xDirectionLooseGap = gap;
   }
 
   /**
@@ -172,18 +152,96 @@ export class BrowserScroll {
     window.scrollTo({ top, left, behavior: behavior ? 'smooth' : 'auto' });
   }
 
-  get scrollTop(): number {
-    return document.documentElement.scrollTop || document.body.scrollTop;
+  /**
+   * 스크롤 y 끝에 도달할 때 알림을 일시 보류한다.
+   * @param {boolean} is
+   */
+  holdDispatchEndY(is: boolean): void {
+    this.yEndDispatchHold = is;
   }
 
-  get scrollHeight(): number {
-    return document.documentElement.scrollHeight || document.body.scrollHeight;
+  /**
+   * 스크롤 x 끝에 도달할 때 알림을 일시 보류한다.
+   * @param {boolean} is
+   */
+  holdDispatchEndX(is: boolean): void {
+    this.xEndDispatchHold = is;
   }
 
-  get innerHeight(): number {
-    return window.innerHeight;
+  /**
+   * 트리거: observeScroll() 의 구독자들에게 알림
+   */
+  protected dispatchScroll(type: ScrollType): void {
+    this.subjectScroll.next(type);
   }
 
+  protected pipeScroll(observable: Observable<ScrollType>, filterType: ScrollType) {
+    return observable.pipe(
+      filter((type) => type === filterType),
+      map(() => this.getState())
+    );
+  }
+
+  /**
+   * 옵저버블: 스크롤이 발생하면 알림
+   * @returns {Observable<IScrollState>}
+   */
+  observeScroll(): Observable<IScrollState> {
+    return this.pipeScroll(this.observableScroll, ScrollType.SCROLL);
+  }
+
+  /**
+   * 옵저버블: 스크롤의 Y 방향이 바뀌면 알림
+   * @returns {Observable<IScrollState>}
+   */
+  observeDirectionY(): Observable<IScrollState> {
+    return this.pipeScroll(this.observableScroll, ScrollType.DIRECTION_Y);
+  }
+
+  /**
+   * 옵저버블: 스크롤의 Y 방향이 바뀌고, 일정 조건에 도달시 발생하는 알림
+   * @returns {Observable<IScrollState>}
+   */
+  observeDirectionLooseY(): Observable<IScrollState> {
+    return this.pipeScroll(this.observableScroll, ScrollType.DIRECTION_LOOSE_Y);
+  }
+
+  /**
+   * 옵저버블: 스크롤이 Y 끝에 도달할 때 알림
+   * @returns {Observable<IScrollState>}
+   */
+  observeEndY(): Observable<IScrollState> {
+    return this.pipeScroll(this.observableScroll, ScrollType.END_Y);
+  }
+
+  /**
+   * 옵저버블: 스크롤의 X 방향이 바뀌면 알림
+   * @returns {Observable<IScrollState>}
+   */
+  observeDirectionX(): Observable<IScrollState> {
+    return this.pipeScroll(this.observableScroll, ScrollType.DIRECTION_X);
+  }
+
+  /**
+   * 옵저버블: 스크롤의 X 방향이 바뀌고, 일정 조건에 도달시 발생하는 알림
+   * @returns {Observable<IScrollState>}
+   */
+  observeDirectionLooseX(): Observable<IScrollState> {
+    return this.pipeScroll(this.observableScroll, ScrollType.DIRECTION_LOOSE_X);
+  }
+
+  /**
+   * 옵저버블: 스크롤이 X 끝에 도달할 때 알림
+   * @returns {Observable<IScrollState>}
+   */
+  observeEndX(): Observable<IScrollState> {
+    return this.pipeScroll(this.observableScroll, ScrollType.END_X);
+  }
+
+  /**
+   * 핸들러: 리사이즈
+   * @protected
+   */
   protected onResize(): void {
     this.onScroll();
   }
@@ -193,77 +251,68 @@ export class BrowserScroll {
    * @protected
    */
   protected onScroll(): void {
-    const currentY = this.currentY;
-    const { scrollTop, endHeight, halfEndHeight, directionY } = this.getState();
+    const yCurrent = this.yCurrent;
+    const xCurrent = this.xCurrent;
+    const { scrollTop, endY, directionY, scrollLeft, endX, directionX } = this.getState();
 
     let yDirection = 0;
-    if (currentY < scrollTop) {
+    let xDirection = 0;
+    if (yCurrent < scrollTop) {
       yDirection = ScrollDirection.DOWN;
-    } else if (scrollTop < currentY) {
+    } else if (scrollTop < yCurrent) {
       yDirection = ScrollDirection.UP;
+    } else {
+      yDirection = ScrollDirection.NONE;
+    }
+    if (xCurrent < scrollLeft) {
+      xDirection = ScrollDirection.RIGHT;
+    } else if (scrollLeft < xCurrent) {
+      xDirection = ScrollDirection.LEFT;
+    } else {
+      xDirection = ScrollDirection.NONE;
     }
 
     this.yDirection = yDirection;
-    this.currentY = scrollTop;
-    this.dispatchScroll();
+    this.xDirection = xDirection;
+    this.yCurrent = scrollTop;
+    this.xCurrent = scrollLeft;
+    this.dispatchScroll(ScrollType.SCROLL);
 
     if (this.yDirection !== directionY) {
-      this.dispatchDirectionY();
+      this.dispatchScroll(ScrollType.DIRECTION_Y);
     }
 
-    if (this.yDirectionLooseBefore !== this.yDirection) {
-      if (this.yDirectionLooseGap <= Math.abs(scrollTop - currentY)) {
-        this.dispatchDirectionLooseY();
-        this.yDirectionLooseBefore = this.yDirection;
-      }
+    if (this.xDirection !== directionX) {
+      this.dispatchScroll(ScrollType.DIRECTION_X);
     }
 
-    if (endHeight <= scrollTop) {
+    if (this.yDirectionLooseBefore !== this.yDirection && this.yDirectionLooseGap <= Math.abs(scrollTop - yCurrent)) {
+      this.dispatchScroll(ScrollType.DIRECTION_LOOSE_Y);
+      this.yDirectionLooseBefore = this.yDirection;
+    }
+
+    if (this.xDirectionLooseBefore !== this.xDirection && this.xDirectionLooseGap <= Math.abs(scrollLeft - xCurrent)) {
+      this.dispatchScroll(ScrollType.DIRECTION_LOOSE_X);
+      this.xDirectionLooseBefore = this.xDirection;
+    }
+
+    if (endY <= scrollTop) {
       if (!this.yEndDispatchHold) {
-        this.dispatchEndY();
+        this.dispatchScroll(ScrollType.END_Y);
       }
       this.holdDispatchEndY(true);
     } else {
       this.holdDispatchEndY(false);
     }
 
-    if (halfEndHeight <= scrollTop) {
-      if (!this.yHalfEndDispatchHold) {
-        this.dispatchHalfEndY();
+    if (endX <= scrollLeft) {
+      if (!this.xEndDispatchHold) {
+        this.dispatchScroll(ScrollType.END_X);
       }
-      this.holdDispatchHalfEndY(true);
+      this.holdDispatchEndX(true);
     } else {
-      this.holdDispatchHalfEndY(false);
+      this.holdDispatchEndX(false);
     }
-  }
-
-  /**
-   * 구독자에게 전달되는 데이터
-   * @returns {IScrollState}
-   */
-  getState(): IScrollState {
-    const scrollTop = this.scrollTop;
-    const scrollHeight = this.scrollHeight;
-    const innerHeight = this.innerHeight;
-    const endHeight = scrollHeight - innerHeight;
-    const halfHeight = innerHeight / 2;
-    const halfEndHeight = endHeight - halfHeight;
-    const directionY = this.yDirection;
-    const holdEndY = this.yEndDispatchHold;
-    const holdHalfY = this.yEndDispatchHold;
-    const percentY = scrollTop / endHeight;
-    return {
-      scrollTop,
-      scrollHeight,
-      innerHeight,
-      endHeight,
-      directionY,
-      halfHeight,
-      halfEndHeight: halfEndHeight < 0 ? 0 : halfEndHeight,
-      holdEndY,
-      holdHalfY,
-      percentY: isNaN(percentY) ? 0 : percentY,
-    };
   }
 
   /**
@@ -274,25 +323,9 @@ export class BrowserScroll {
       this.eventListener.unsubscribe();
       this.eventListener = null;
     }
-
-    if (this.scrollSubject) {
-      this.scrollSubject.unsubscribe();
-      this.scrollSubject = null;
-    }
-
-    if (this.yDirectionLooseSubject) {
-      this.yDirectionLooseSubject.unsubscribe();
-      this.yDirectionLooseSubject = null;
-    }
-
-    if (this.yEndSubject) {
-      this.yEndSubject.unsubscribe();
-      this.yEndSubject = null;
-    }
-
-    if (this.yHalfEndSubject) {
-      this.yHalfEndSubject.unsubscribe();
-      this.yHalfEndSubject = null;
+    if (this.subjectScroll) {
+      this.subjectScroll.unsubscribe();
+      this.subjectScroll = null;
     }
   }
 }

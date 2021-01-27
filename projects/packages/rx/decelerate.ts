1320441530722edbb81c5a5cb5cbc08686172dd7
이윤석 <ys84@wemakeprice.com>
2021-01-27 12:28:39 +0900

    rx decelerate 추가

--- projects/packages/rx/decelerate.ts
+++ projects/packages/rx/decelerate.ts
@@ -1,13 +1,35 @@
+/**
+ * @packageDocumentation
+ * @module rx
+ */
 import { Observable, Subject } from 'rxjs';
 
-interface Config {
+/**
+ * 감속 설정값
+ * @interface
+ * @property start {number} 시작 값
+ * @property end {number} 도달하려는 값
+ * @property friction {number} 0 < n <= 1 감속 마찰값(높을 수록 빠르게 도달)
+ * @property delay {number} 감속 계산 사이 대기시간(ms)
+ */
+interface DecelerateConfig {
   start: number;
   end: number;
   friction?: number;
   delay?: number;
 }
 
-export function decelerate(config: Config): Observable<number> {
+/**
+ * start(시작값) 부터 end(도달값) 까지 friction(마찰값)에 영향을 받아 감속하며 도달
+ * @export
+ * @param {DecelerateConfig} config
+ * @returns {Observable<number>}
+ * @example
+ *  decelerate({ start: 0, end: 100, friction: 0.1, delay: 0 }).subscribe(value => {
+ *    console.log(value); // 0 ~ ...... ~ 100
+ *  });
+ */
+export function decelerate(config: DecelerateConfig): Observable<number> {
   const { start: targetStart, end: targetEnd, friction = 0.1, delay = 0 } = config;
   if (friction <= 0) {
     throw new Error(`Friction can't be set below zero.`);
